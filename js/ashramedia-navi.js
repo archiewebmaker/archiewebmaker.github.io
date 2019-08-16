4bc0bb307200f7fc1d150020e5c8a88a6a850803
ashramedia <52618405+ashramedia@users.noreply.github.com>
2019-08-17 00:22:00 +0530

    Update ashramedia-navi.js

--- js/ashramedia-navi.js
+++ js/ashramedia-navi.js
@@ -1,4 +1,176 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('W 1b(D){3 8=H;3 c=U 13();3 7=1;3 9=1;3 r=0;3 A=0;3 w=0;3 5=\'\';3 m=\'\';3 v=\'\';y(3 i=0,h;h=D.10.11[i];i++){3 O=h.L.$t.B(0,19)+h.L.$t.B(Z,V);C=X(O);3 s=h.s.$t;4(s!=\'\'){4(r==0||(r%u==(u-1))){4(8.d(C)!=-1){7=9}4(s!=\'\')9++;c[c.l]=\'/j?J-g=\'+C+\'&g-G=\'+u}}r++}y(3 p=0;p<c.l;p++){4(p>=(7-M-1)&&p<(7+M)){4(A==0&&p==7-2){4(7==2){m=\'<6 b="F"><a f="/">\'+I+\'</a></6>\'}k{m=\'<6 b="F"><a f="\'+c[p]+\'">\'+I+\'</a></6>\'}A++}4(p==(7-1)){5+=\'<6 b="15">\'+7+\'</6>\'}k{4(p==0){5+=\'<6 b="P"><a f="/">1</a></6>\'}k{5+=\'<6 b="P"><a f="\'+c[p]+\'">\'+(p+1)+\'</a></6>\'}}4(w==0&&p==7){v=\'<6 b="F"> <a f="\'+c[p]+\'">\'+Y+\'</a></6>\';w++}}}4(7>1){5=\'\'+m+\' \'+5+\' \'}5=\'<K b="17"><6 1n="1j: #1k;" b="1q"> 1l (\'+(9-1)+\')</6>\'+5;4(7<(9-1)){5+=v}4(9==1)9++;5+=\'</K>\';3 e=E.1m("e");3 z=E.1o("16-1i");4(9<=2){5=\'\'}y(3 p=0;p<e.l;p++){e[p].N=5}4(e&&e.l>0){5=\'\'}4(z){z.N=5}}W 1g(D){3 8=H;3 c=U 13();3 Q=8.d("/j/o/")!=-1;3 n=Q?8.12(8.d("/j/o/")+14,8.l):"";n=n.d("?")!=-1?n.12(0,n.d("?")):n;3 7=1;3 9=1;3 r=0;3 A=0;3 w=0;3 5=\'\';3 m=\'\';3 v=\'\';3 R=\'<6 b="P"><a f="/j/o/\'+n+\'?&g-G=\'+u+\'">\';3 8=H;y(3 i=0,h;h=D.10.11[i];i++){3 O=h.L.$t.B(0,19)+h.L.$t.B(Z,V);C=X(O);3 s=h.s.$t;4(s!=\'\'){4(r==0||(r%u==(u-1))){4(8.d(C)!=-1){7=9}4(s!=\'\')9++;c[c.l]=\'/j/o/\'+n+\'?J-g=\'+C+\'&g-G=\'+u}}r++}y(3 p=0;p<c.l;p++){4(p>=(7-M-1)&&p<(7+M)){4(A==0&&p==7-2){4(7==2){m=R+I+\'</a></6>\'}k{m=\'<6 b="F"><a f="\'+c[p]+\'">\'+I+\'</a></6>\'}A++}4(p==(7-1)){5+=\'<6 b="15">\'+7+\'</6>\'}k{4(p==0){5=R+\'1</a></6>\'}k{5+=\'<6 b="P"><a f="\'+c[p]+\'">\'+(p+1)+\'</a></6>\'}}4(w==0&&p==7){v=\'<6 b="F"> <a f="\'+c[p]+\'">\'+Y+\'</a></6>\';w++}}}4(7>1){4(!Q){5=\'\'+m+\' \'+5+\' \'}k{5=\'\'+m+\' \'+5+\' \'}}5=\'<K b="17"><6 1n="1j: #1k;" b="1q"> 1l (\'+(9-1)+\')</6>\'+5;4(7<(9-1)){5+=v}4(9==1)9++;5+=\'</K>\';3 e=E.1m("e");3 z=E.1o("16-1i");4(9<=2){5=\'\'}y(3 p=0;p<e.l;p++){e[p].N=5}4(e&&e.l>0){5=\'\'}4(z){z.N=5}}3 H=1s.f;3 8=H;4(8.d("/j/o/")!=-1){4(8.d("?J-g")!=-1){3 S=8.B(8.d("/j/o/")+14,8.d("?J-g"))}k{3 S=8.B(8.d("/j/o/")+14,8.d("?&g"))}}3 T="/";4(8.d("?q=")==-1){4(8.d("/j/o/")==-1){E.1c(\'<x 1f="\'+T+\'1e/1d/1t?18=D-1a-x&1h=1b&g-G=1p" ><\\/x>\')}k{E.1c(\'<x 1f="\'+T+\'1e/1d/1r/-/\'+S+\'?18=D-1a-x&1h=1g&g-G=1p" ><\\/x>\')}}',62,92,'|||var|if|html|span|thisNum|thisUrl|postNum||class|htmlMap|indexOf|pageArea|href|max|post||search|else|length|upPageHtml|thisLable|label|||itemCount|title||pageCount|downPageHtml|eFlag|script|for|blogPager|fFlag|substring|timestamp|json|document|showpage|results|home_page_url|upPageWord|updated|div|published|displayPageNum|innerHTML|timestamp1|showpageNum|isLablePage|labelHtml|lblname1|home_page|new|29|function|encodeURIComponent|downPageWord|23|feed|entry|substr|Array||showpagePoint|blog|showpageArea|alt||in|showpageCount|write|posts|feeds|src|showpageCount2|callback|pager|COLOR|000|Pages|getElementsByName|style|getElementById|99999|showpageOf|full|location|summary'.split('|'),0,{}))
+// Page Navigation
+function showpageCount(json) {
+    var thisUrl = home_page_url;
+    var htmlMap = new Array();
+    var thisNum = 1;
+    var postNum = 1;
+    var itemCount = 0;
+    var fFlag = 0;
+    var eFlag = 0;
+    var html = '';
+    var upPageHtml = '';
+    var downPageHtml = '';
+    for (var i = 0, post; post = json.feed.entry[i]; i++) {
+        var timestamp1 = post.published.$t.substring(0, 19) + post.published.$t.substring(23, 29);
+        timestamp = encodeURIComponent(timestamp1);
+        var title = post.title.$t;
+        if (title != '') {
+            if (itemCount == 0 || (itemCount % pageCount == (pageCount - 1))) {
+                if (thisUrl.indexOf(timestamp) != -1) {
+                    thisNum = postNum
+                }
+                if (title != '') postNum++;
+                htmlMap[htmlMap.length] = '/search?updated-max=' + timestamp + '&max-results=' + pageCount
+            }
+        }
+        itemCount++
+    }
+    for (var p = 0; p < htmlMap.length; p++) {
+        if (p >= (thisNum - displayPageNum - 1) && p < (thisNum + displayPageNum)) {
+            if (fFlag == 0 && p == thisNum - 2) {
+                if (thisNum == 2) {
+                    upPageHtml = '<span class="showpage"><a href="/">' + upPageWord + '</a></span>'
+                } else {
+                    upPageHtml = '<span class="showpage"><a href="' + htmlMap[p] + '">' + upPageWord + '</a></span>'
+                }
+                fFlag++
+            }
+            if (p == (thisNum - 1)) {
+                html += '<span class="showpagePoint">' + thisNum + '</span>'
+            } else {
+                if (p == 0) {
+                    html += '<span class="showpageNum"><a href="/">1</a></span>'
+                } else {
+                    html += '<span class="showpageNum"><a href="' + htmlMap[p] + '">' + (p + 1) + '</a></span>'
+                }
+            } if (eFlag == 0 && p == thisNum) {
+                downPageHtml = '<span class="showpage"> <a href="' + htmlMap[p] + '">' + downPageWord + '</a></span>';
+                eFlag++
+            }
+        }
+    }
+    if (thisNum > 1) {
+        html = '' + upPageHtml + ' ' + html + ' '
+    }
+    html = '<div class="showpageArea"><span style="COLOR: #000;" class="showpageOf"> Pages (' + (postNum - 1) + ')</span>' + html;
+    if (thisNum < (postNum - 1)) {
+        html += downPageHtml
+    }
+    if (postNum == 1) postNum++;
+    html += '</div>';
+    var pageArea = document.getElementsByName("pageArea");
+    var blogPager = document.getElementById("blog-pager");
+    if (postNum <= 2) {
+        html = ''
+    }
+    for (var p = 0; p < pageArea.length; p++) {
+        pageArea[p].innerHTML = html
+    }
+    if (pageArea && pageArea.length > 0) {
+        html = ''
+    }
+    if (blogPager) {
+        blogPager.innerHTML = html
+    }
+}
 
-
-eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('$(g).f(3(){3 1(){e.c.d(\'7://a.b.9/\')}3 4(){2($(\'#h\').8===0){1()}5 2($(\'#6\').8===0){1()}5 2($("#6").m("l")!=="7://a.b.9/"){1()}5 2($(\'#6\').k()!=="j"){1()}}4();i(3(){4()},n)});',24,24,'|redirect|if|function|check|else|creditlink|https|length|com|www|ashramedia|location|assign|window|ready|document|credits|setInterval|Ashramedia|text|href|attr|5000'.split('|'),0,{}))
+function showpageCount2(json) {
+    var thisUrl = home_page_url;
+    var htmlMap = new Array();
+    var isLablePage = thisUrl.indexOf("/search/label/") != -1;
+    var thisLable = isLablePage ? thisUrl.substr(thisUrl.indexOf("/search/label/") + 14, thisUrl.length) : "";
+    thisLable = thisLable.indexOf("?") != -1 ? thisLable.substr(0, thisLable.indexOf("?")) : thisLable;
+    var thisNum = 1;
+    var postNum = 1;
+    var itemCount = 0;
+    var fFlag = 0;
+    var eFlag = 0;
+    var html = '';
+    var upPageHtml = '';
+    var downPageHtml = '';
+    var labelHtml = '<span class="showpageNum"><a href="/search/label/' + thisLable + '?&max-results=' + pageCount + '">';
+    var thisUrl = home_page_url;
+    for (var i = 0, post; post = json.feed.entry[i]; i++) {
+        var timestamp1 = post.published.$t.substring(0, 19) + post.published.$t.substring(23, 29);
+        timestamp = encodeURIComponent(timestamp1);
+        var title = post.title.$t;
+        if (title != '') {
+            if (itemCount == 0 || (itemCount % pageCount == (pageCount - 1))) {
+                if (thisUrl.indexOf(timestamp) != -1) {
+                    thisNum = postNum
+                }
+                if (title != '') postNum++;
+                htmlMap[htmlMap.length] = '/search/label/' + thisLable + '?updated-max=' + timestamp + '&max-results=' + pageCount
+            }
+        }
+        itemCount++
+    }
+    for (var p = 0; p < htmlMap.length; p++) {
+        if (p >= (thisNum - displayPageNum - 1) && p < (thisNum + displayPageNum)) {
+            if (fFlag == 0 && p == thisNum - 2) {
+                if (thisNum == 2) {
+                    upPageHtml = labelHtml + upPageWord + '</a></span>'
+                } else {
+                    upPageHtml = '<span class="showpage"><a href="' + htmlMap[p] + '">' + upPageWord + '</a></span>'
+                }
+                fFlag++
+            }
+            if (p == (thisNum - 1)) {
+                html += '<span class="showpagePoint">' + thisNum + '</span>'
+            } else {
+                if (p == 0) {
+                    html = labelHtml + '1</a></span>'
+                } else {
+                    html += '<span class="showpageNum"><a href="' + htmlMap[p] + '">' + (p + 1) + '</a></span>'
+                }
+            } if (eFlag == 0 && p == thisNum) {
+                downPageHtml = '<span class="showpage"> <a href="' + htmlMap[p] + '">' + downPageWord + '</a></span>';
+                eFlag++
+            }
+        }
+    }
+    if (thisNum > 1) {
+        if (!isLablePage) {
+            html = '' + upPageHtml + ' ' + html + ' '
+        } else {
+            html = '' + upPageHtml + ' ' + html + ' '
+        }
+    }
+    html = '<div class="showpageArea"><span style="COLOR: #000;" class="showpageOf"> Pages (' + (postNum - 1) + ')</span>' + html;
+    if (thisNum < (postNum - 1)) {
+        html += downPageHtml
+    }
+    if (postNum == 1) postNum++;
+    html += '</div>';
+    var pageArea = document.getElementsByName("pageArea");
+    var blogPager = document.getElementById("blog-pager");
+    if (postNum <= 2) {
+        html = ''
+    }
+    for (var p = 0; p < pageArea.length; p++) {
+        pageArea[p].innerHTML = html
+    }
+    if (pageArea && pageArea.length > 0) {
+        html = ''
+    }
+    if (blogPager) {
+        blogPager.innerHTML = html
+    }
+}
+var home_page_url = location.href;
+var thisUrl = home_page_url;
+if (thisUrl.indexOf("/search/label/") != -1) {
+    if (thisUrl.indexOf("?updated-max") != -1) {
+        var lblname1 = thisUrl.substring(thisUrl.indexOf("/search/label/") + 14, thisUrl.indexOf("?updated-max"))
+    } else {
+        var lblname1 = thisUrl.substring(thisUrl.indexOf("/search/label/") + 14, thisUrl.indexOf("?&max"))
+    }
+}
+var home_page = "/";
+if (thisUrl.indexOf("?q=") == -1) {
+    if (thisUrl.indexOf("/search/label/") == -1) {
+        document.write('<script src="' + home_page + 'feeds/posts/summary?alt=json-in-script&callback=showpageCount&max-results=99999" ><\/script>')
+    } else {
+        document.write('<script src="' + home_page + 'feeds/posts/full/-/' + lblname1 + '?alt=json-in-script&callback=showpageCount2&max-results=99999" ><\/script>')
+    }
+}
